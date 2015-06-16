f70cf3e8b3525bfff57b7aa54f47b0b0dd098b18
matmax <matthias.graf@st.ovgu.de>
2015-06-16 17:29:51 +0200

    histogram and tag cloud alpha

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 
-var vikus = (function() {
-	var vikus = {}
+var vikus = (function(vikus = {}) {
 
 	vikus.init = function() {
 		//loadJson("data/spsg_NEU.json", withData)
@@ -13,10 +12,11 @@ var vikus = (function() {
 			cat: {path: "data/kategorien_aus_ausstellungskatalog.json"},
 			spsg: {path: "data/spsg.json"}
 		}
-		if (false)
+		if (true)
 			loadJsonFilesAndSync(fields, withData)
 
-		draw()
+		if (false)
+			draw()
 	}
 
 	Object.values = function(obj) {
@@ -25,6 +25,10 @@ var vikus = (function() {
 		})
 	}
 
+	console.logToHTML = function(x) {
+		document.body.appendChild(document.createTextNode(x))
+		document.body.appendChild(document.createElement("BR"))
+	}
 
 	function withData(fields) {
 		var obj = fields.spsg.data
@@ -55,6 +59,14 @@ var vikus = (function() {
 		Object.keys(obj).forEach(function(key) {
 			console.assert(key === obj[key].id)
 		})
+
+		// filter out those without a year
+		Object.values(obj).forEach(function(e) {
+			if (!e.jahr) {
+				delete obj[e.id]
+			}
+		})
+
 
 		Object.values(obj).forEach(function(e) {
 			console.assert(e.inventar_nr.match("GK II \\(12\\) "))
@@ -115,8 +127,8 @@ var vikus = (function() {
 		var tagSet = new MySet()
 		var countTagsSet = new MySet()
 		Object.values(obj).forEach(function(e) {
+			e.tags = []
 			if (e.index) {
-				e.tags = []
 				var z = e.index.split("; ")
 				z.forEach(function(s) {
 					var y = s.split(": ")
@@ -151,8 +163,12 @@ var vikus = (function() {
 		Object.values(obj).forEach(function(e) {
 			if (e.zusammenhang) {
 				countZusammenhang++
-				for (var ref of e.zusammenhang.split("; "))
-					console.assert(obj[ref])
+				var parsedZusammenhangIds = e.zusammenhang.split("; ")
+				e.zusammenhang = []
+				for (var ref of parsedZusammenhangIds) {
+					if (ref && obj[ref])
+						e.zusammenhang.push(obj[ref])
+				}
 			}
 		})
 		console.log("einträge mit zusammenhang: "+countZusammenhang)
@@ -169,15 +185,66 @@ var vikus = (function() {
 				img.src = url
 			})
 
-		if (false)
-			Object.values(obj).forEach(function(e) {
-				if (e.zeit) {
-					console.log(e.zeit)
+
+
+		// 1810
+		var kleinsteZeit = Object.values(obj).map(e => e.jahr).filter(e => e).reduce((a,b) => a < b ? a : b)
+		// 1856
+		var groesteZeit = Object.values(obj).map(e => e.jahr).filter(e => e).reduce((a,b) => a > b ? a : b)
+
+		console.log(kleinsteZeit)
+		console.log(groesteZeit)
+
+
+
+
+
+		if (false) {
+		var svgViewboxX = -250
+		var svgViewboxY = -250
+		var svgViewboxWidth = 500
+		var svgViewboxHeight = 500
+
+		var svg = d3.select("#vis")
+			.append("svg")
+			.attr("xmlns", "http://www.w3.org/2000/svg")
+			.attr("viewBox", svgViewboxX+" "+svgViewboxY+" "+svgViewboxWidth+" "+svgViewboxHeight)
+			var result = []
+			var count = 0
+			tagSet.getSortedArray().forEach(function([tag, frequency]) {
+				var jahre = Object.values(obj).filter(e => e.tags.indexOf(tag) !== -1).map(e => e.jahr)
+				var min = jahre.reduce((a,b) => a < b ? a : b, 2000)
+				var max = jahre.reduce((a,b) => a > b ? a : b, 0)
+				var sum = jahre.reduce((a,b) => a+b, 0)
+				console.assert(min !== 2000 && max !== 0)
+
+				if (jahre.length > 10) {
+					result.push([tag, jahre.length, min, max, Math.floor((max-min)/46*100)+"%"])
+
+					var scale = 1+Math.log(1+jahre.length/643)/Math.log(2)
+
+					var text = svg.append("text")
+						.attr("x", (sum/jahre.length-1810)*30-500)
+						//.attr("y", -100+6*count++)
+						.attr("y", 100)
+						.attr("text-anchor", "middle")
+						.attr("transform", "scale("+scale+")")
+						.text(tag)
+
+
+					//result.push(tag+":"+jahre.length)
+					//document.body.appendChild(document.createTextNode(tag+": "+sum/jahre.length))
+					//document.body.appendChild(document.createElement("BR"))
 				}
 			})
+			//vikus.matrixToTable(result)
+		}
 
 
 
+
+
+		histogram(obj)
 
 
 
@@ -219,37 +286,89 @@ var vikus = (function() {
 
 
 
-	function MySet() {
-		var self = this
-		self.entriesAndFrequency = new Map()
+
+
+
+
+
+	function histogram(obj) {
+		var svgViewboxX = -250
+		var svgViewboxY = -250
+		var svgViewboxWidth = 500
+		var svgViewboxHeight = 500
+
+		var svg = d3.select("#vis")
+			.append("svg")
+			.attr("xmlns", "http://www.w3.org/2000/svg")
+			.attr("viewBox", svgViewboxX+" "+svgViewboxY+" "+svgViewboxWidth+" "+svgViewboxHeight)
+
+		var g = svg.append("g").attr("transform", "translate(-500,-250) scale(10)")
+
+		var text = svg.append("text")
+			.attr("x", 220).attr("y", -230)
+			.text("position")
+
+		var histWidth = 100
+		var histHeight = 50
+		g.append("rect").attr({x: 0, y: 0, width: histWidth, height: histHeight})
+			.style({fill: "#000", "fill-opacity": 0.05})
+
+		svg.on("mousemove", function() {
+			var mouse = d3.mouse(svg.node())
+			text.text(Math.floor(mouse[0])+" x "+Math.floor(mouse[1]))
+		})
+
+		var startYear = 1810
+		var endYear = 1856
+		var numberOfBars = 46
+
+		var sortedByYear = Object.values(obj).sort(function(a,b) {
+			return a.jahr > b.jahr ? 1 : -1
+		})
+
+		var yearHistogram = new MySet()
+
+		sortedByYear.forEach(function(e) {
+			console.assert(e.jahr)
+			yearHistogram.add(e.jahr)
+		})
+
+		// produktivste jahre:
+		console.log(yearHistogram.getSorted())
+
+		var currentYear = 1810
+		var currentAmount = 0
+		var numberOfInBarColumns = 4
+		var columnWidthTotal = histWidth/numberOfBars
+		var columnMargin = 0.8
+		var columnWidth = columnWidthTotal*columnMargin
+
+		var blockTotal = columnWidth/numberOfInBarColumns
+		var blockMargin = 0.8
+		var block = blockTotal*blockMargin
+
+		sortedByYear.forEach(function(e) {
+			var totalPerYear = yearHistogram.get(e.jahr)
+			var totalPerInBarColumn = Math.ceil(totalPerYear/numberOfInBarColumns)
+			if (e.jahr === currentYear) {
+				currentAmount++
+			} else {
+				currentYear = e.jahr
+				currentAmount = 0
+			}
+
+			var inBarColumnNo = Math.floor(currentAmount/totalPerInBarColumn)
+
+			var x = (currentYear-startYear)/(endYear-startYear+1)*histWidth + inBarColumnNo*blockTotal
+			var y = histHeight-(1+currentAmount-inBarColumnNo*totalPerInBarColumn) *blockTotal
+
+			g.append("rect").attr({x: x, y: y, width: block, height: block})
+				.style({fill: "#009", "fill-opacity": 0.3})
+		})
+
+
 	}
 
-	MySet.prototype.add = function(e) {
-		var self = this
-		if (self.entriesAndFrequency.has(e)) {
-			self.entriesAndFrequency.set(e, self.entriesAndFrequency.get(e)+1)
-		} else {
-			self.entriesAndFrequency.set(e, 1)
-		}
-	}
-
-	MySet.prototype.get = function() {
-		var self = this
-		return self.entriesAndFrequency
-	}
-
-	MySet.prototype.getSorted = function() {
-		var self = this
-		// harmony solution:
-		//var result = [...self.entriesAndFrequency].sort(([k1, v1], [k2, v2]) => v1 < v2 ? 1 : -1)
-		//return result.map(([key, entry]) => key+": "+entry)
-
-		var result = []
-		for (var e of self.entriesAndFrequency.entries())
-			result.push(e)
-		result.sort(function(e1, e2) { return e1[1] < e2[1] ? 1 : -1 })
-		return result.map(function(e) { return e[0]+": "+e[1] })
-	}
 
 
 
@@ -331,10 +450,57 @@ var vikus = (function() {
 
 
 
+	function MySet() {
+		var self = this
+		self.entriesAndFrequency = new Map()
+	}
 
+	MySet.prototype.add = function(e) {
+		var self = this
+		if (self.entriesAndFrequency.has(e)) {
+			self.entriesAndFrequency.set(e, self.entriesAndFrequency.get(e)+1)
+		} else {
+			self.entriesAndFrequency.set(e, 1)
+		}
+	}
+
+	MySet.prototype.get = function(e) {
+		var self = this
+		return e ? self.entriesAndFrequency.get(e) : self.entriesAndFrequency
+	}
+
+	MySet.prototype.getAsArray = function() {
+		var self = this
+		var result = []
+		for (var e of self.entriesAndFrequency.entries())
+			result.push(e[0])
+		return result
+	}
+
+	MySet.prototype.getSortedArray = function() {
+		var self = this
+		var result = []
+		for (var e of self.entriesAndFrequency.entries())
+			result.push(e)
+		result.sort(function(e1, e2) { return e1[1] < e2[1] ? 1 : -1 })
+		return result
+	}
+
+	MySet.prototype.getSorted = function() {
+		var self = this
+		// harmony solution:
+		//var result = [...self.entriesAndFrequency].sort(([k1, v1], [k2, v2]) => v1 < v2 ? 1 : -1)
+		//return result.map(([key, entry]) => key+": "+entry)
+
+		var result = []
+		for (var e of self.entriesAndFrequency.entries())
+			result.push(e)
+		result.sort(function(e1, e2) { return e1[1] < e2[1] ? 1 : -1 })
+		return result.map(function(e) { return e[0]+": "+e[1] })
+	}
 
 
 
 
 	return vikus
-})()
+})(vikus)
