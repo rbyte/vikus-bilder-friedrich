5916ab017ff364c803dffb95c4c986457b18bd25
matmax <matthias.graf@st.ovgu.de>
2015-06-30 20:26:43 +0200

    tag cloud und histogram upgrade

--- js/main.js
+++ js/main.js
@@ -16,20 +16,22 @@ var vikus = (function(vikus = {}) {
 	var svgViewboxY = -defaultSvgViewboxHeight/2
 	var svgViewboxWidth = defaultSvgViewboxHeight
 	var svgViewboxHeight = defaultSvgViewboxHeight
-	var zoomFactor = 1.15 // macs tend to have finer grain mouse wheel ticks
+	var zoomFactor = 1.25 // macs tend to have finer grain mouse wheel ticks
 	var zoomTransitionDuration = 150
 
-	var histObj = {
-		histWidth: 100,
-		histHeight: 50,
-		startYear: 1810,
-		endYear: 1856,
-		numberOfBars: 46,
-		numberOfInBarColumns: 4,
-		columnMargin: 0.8,
-		blockMargin: 0.8
-	}
+	var obj
+	var cat
 
+	var histG
+	var histWidth = 100
+	var histHeight = 25
+	var numberOfInBarColumns = 4
+	var columnMargin = 0.8
+	var blockMargin = 0.8
+
+	var tagCloudG
+
+	var tagSet = new MySet()
 
 	vikus.init = function() {
 		var fields = {
@@ -89,8 +91,8 @@ var vikus = (function(vikus = {}) {
 
 
 	function withData(fields) {
-		var obj = fields.spsg.data
-		var cat = fields.cat.data
+		obj = fields.spsg.data
+		cat = fields.cat.data
 
 		var anzahlEintraege = Object.keys(obj).length
 
@@ -182,7 +184,7 @@ var vikus = (function(vikus = {}) {
 		})
 		console.assert(misses === 0)
 
-		var tagSet = new MySet()
+
 		var countTagsSet = new MySet()
 		Object.values(obj).forEach(function(e) {
 			e.tags = []
@@ -255,43 +257,8 @@ var vikus = (function(vikus = {}) {
 
 
 
-
-
-		if (false) {
-			var result = []
-			var count = 0
-			tagSet.getSortedArray().forEach(function([tag, frequency]) {
-				var jahre = Object.values(obj).filter(e => e.tags.indexOf(tag) !== -1).map(e => e.jahr)
-				var min = jahre.reduce((a,b) => a < b ? a : b, 2000)
-				var max = jahre.reduce((a,b) => a > b ? a : b, 0)
-				var sum = jahre.reduce((a,b) => a+b, 0)
-				console.assert(min !== 2000 && max !== 0)
-
-				if (jahre.length > 10) {
-					result.push([tag, jahre.length, min, max, Math.floor((max-min)/46*100)+"%"])
-
-					var scale = 1+Math.log(1+jahre.length/643)/Math.log(2)
-
-					var text = svg.append("text")
-						.attr("x", (sum/jahre.length-1810)*30-500)
-						//.attr("y", -100+6*count++)
-						.attr("y", 100)
-						.attr("text-anchor", "middle")
-						.attr("transform", "scale("+scale+")")
-						.text(tag)
-
-
-					//result.push(tag+":"+jahre.length)
-					//document.body.appendChild(document.createTextNode(tag+": "+sum/jahre.length))
-					//document.body.appendChild(document.createElement("BR"))
-				}
-			})
-			//vikus.matrixToTable(result)
-		}
-
-
 		var text = svg.append("text")
-			.attr("x", 220).attr("y", -230)
+			.attr("x", svgViewboxX+svgViewboxWidth-100).attr("y", svgViewboxY+20)
 			.text("position")
 
 		svg.on("mousemove", function() {
@@ -299,8 +266,8 @@ var vikus = (function(vikus = {}) {
 			text.text(Math.floor(mouse[0])+" x "+Math.floor(mouse[1]))
 		})
 
-		histObj.obj = obj
-		histObj.draw()
+		draw()
+		tagCloud()
 
 		console.log("done.")
 	}
@@ -349,17 +316,20 @@ var vikus = (function(vikus = {}) {
 
 			var xDelta = svgViewboxWidth * (wheelMovement < 0 ? zoomFactor-1 : -(1-1/zoomFactor))
 			var yDelta = svgViewboxHeight * (wheelMovement < 0 ? zoomFactor-1 : -(1-1/zoomFactor))
+
+			// keep bottom line stable!
+			var bottom = svgViewboxY+svgViewboxHeight
+
 			// zoom towards the current mouse position
 			var relX = (mouse[0]-svgViewboxX)/svgViewboxWidth // in [0,1]
 			var relY = (mouse[1]-svgViewboxY)/svgViewboxHeight // in [0,1]
-			svgViewboxX -= xDelta * relX
-			svgViewboxY -= yDelta * relY
 			svgViewboxWidth += xDelta
 			svgViewboxHeight += yDelta
+			svgViewboxX -= xDelta * relX
+			svgViewboxY = bottom - svgViewboxHeight
 			d3.event = null
 
-			histObj.numberOfInBarColumns += wheelMovement
-			histObj.draw()
+			draw()
 
 			updateViewbox(true/* with transition */)
 		}
@@ -372,33 +342,25 @@ var vikus = (function(vikus = {}) {
 		svg.call(d3.behavior.drag()
 			.on("drag", function (d) {
 				svgViewboxX -= d3.event.dx*(svgViewboxWidth/svgWidth)
-				svgViewboxY -= d3.event.dy*(svgViewboxHeight/svgHeight)
+					// keep bottom line stable!
+				//svgViewboxY -= d3.event.dy*(svgViewboxHeight/svgHeight)
 				updateViewbox()
 			})
 		)
 	}
 
 
-	histObj.draw = function() {
-		var self = this
-		var obj = self.obj
-
-		var histWidth = self.histWidth
-		var histHeight = self.histHeight
-		var startYear = self.startYear
-		var endYear = self.endYear
-		var numberOfBars = self.numberOfBars
-		var numberOfInBarColumns = self.numberOfInBarColumns
-		var columnMargin = self.columnMargin
-		var blockMargin = self.blockMargin
-
+	function draw() {
+		var startYear = 1810
+		var endYear = 1856
+		var numberOfBars = 46
 		var currentYear = 1810
 		var currentAmount = 0
 
 		var g
-		if (!histObj.g) {
-			g = histObj.g = svg.append("g").attr("transform", "translate(-500,-250) scale(10)")
-			g.append("rect").attr({x: 0, y: 0, width: histWidth, height: histHeight})
+		if (!histG) {
+			histG = svg.append("g").attr("transform", "translate(-500,-10) scale(10)")
+			histG.append("rect").attr({x: 0, y: 0, width: histWidth, height: histHeight})
 				.style({fill: "#000", "fill-opacity": 0.05})
 		}
 
@@ -435,88 +397,61 @@ var vikus = (function(vikus = {}) {
 			var x = (currentYear-startYear)/(endYear-startYear+1)*histWidth + inBarColumnNo*blockTotal
 			var y = histHeight-(1+currentAmount-inBarColumnNo*totalPerInBarColumn) *blockTotal
 
-			var rect = e.histogramRect ? e.histogramRect : g.append("rect")
+			e.histogramRect = e.histogramRect ? e.histogramRect : histG.append("rect")
 
-			e.histogramRect = rect
+			e.histogramRect.transition().duration(900)
 				.attr({x: x, y: y, width: block, height: block})
 				.style({fill: "#009", "fill-opacity": 0.3})
+
 		})
 	}
 
-
-
-
-	function draw() {
-		var svgViewboxX = -250
-		var svgViewboxY = -250
-		var svgViewboxWidth = 500
-		var svgViewboxHeight = 500
-
-		var svg = d3.select("#vis")
-			.append("svg")
-			.attr("xmlns", "http://www.w3.org/2000/svg")
-			.attr("viewBox", svgViewboxX+" "+svgViewboxY+" "+svgViewboxWidth+" "+svgViewboxHeight)
-
-		svg.append("rect")
-			.attr("x", -250).attr("y", -250).attr("width", "500px").attr("height", "500px")
-			.style({fill: "#000", "fill-opacity": 0.05})
-
+	function tagCloud() {
+		var result = []
 		var count = 0
-		Object.values(skizzen).forEach(function(e) {
-			count++
-			//if (count > 100)
-			//	return
-			var coords = layout[e.id]
-			//svg.append("rect")
-			//	.attr("x", coords[0]+"px")
-			//	.attr("y", coords[1]+"px")
-			//	.attr("width", 15+"px")
-			//	.attr("height", 15+"px")
-			//	.style({fill: "#f00", "fill-opacity": 0.2})
 
-			svg.append("image")
-				.attr("xlink:href", "data/bilder_100/"+e.id+".jpg")
-				.attr("x", coords[0]+"px")
-				.attr("y", coords[1]+"px")
-				.attr("width", 15+"px")
-				.attr("height", 15+"px")
-				.on("click", function (d, i) {
+		tagCloudG = tagCloudG ? tagCloudG : svg.append("g")
+		// .attr("transform", "translate(-500,-10) scale(10)")
 
-				})
+		tagSet.getSortedArray().forEach(function([tag, frequency]) {
+			var jahre = Object.values(obj).filter(e => e.tags.indexOf(tag) !== -1).map(e => e.jahr)
+
+			var startYear = 1810
+			var endYear = 1856
+			var min = jahre.reduce((a,b) => a < b ? a : b, 2000)
+			var max = jahre.reduce((a,b) => a > b ? a : b, 0)
+			console.assert(min >= startYear && max <= endYear)
+			var sum = jahre.reduce((a,b) => a+b, 0)
+			var meanYear = sum/jahre.length
+			var meanYearNormalised = (meanYear-startYear)/(endYear-startYear)
+			var xDomain = svgViewboxWidth*0.8
+			console.assert(0 <= meanYearNormalised && meanYearNormalised <= 1)
+
+			var myPow = (x,y) => x < 0 ? -Math.pow(Math.abs(x), 1/y) : Math.pow(x, y)
+			var strech = x => myPow((x-.5)/.5, 2)*.5+.5
+			//var strech = x => x
+			var yVal = strech(meanYearNormalised)
+			console.assert(!isNaN(yVal))
+			console.assert(0 <= yVal && yVal <= 1)
+
+			if (jahre.length > 20) {
+				result.push([tag, jahre.length, min, max, Math.floor((max-min)/46*100)+"%"])
+
+				var scale = 1+Math.log(1+jahre.length/643)/Math.log(2)
+
+				var text = tagCloudG.append("text")
+					.attr("x", yVal*xDomain-xDomain/2)
+					.attr("y", -100+1.5*count++)
+					.attr("font-size", 6)
+					//.attr("y", 0)
+					.attr("text-anchor", "middle")
+					.attr("transform", "scale("+scale+")")
+					.text(tag)
+			}
 		})
-
-		var zoomFactor = 1.15 // macs tend to have finer grain mouse wheel ticks
-
-		function zoom(event) {
-			var wheelMovement = Math.max(-1, Math.min(1, (event.wheelDelta || -event.detail)))
-			// ok, I cheated a bit ...
-			d3.event = event
-			var mouse = d3.mouse(domSvg)
-
-			var xDelta = svgViewboxWidth * (wheelMovement < 0 ? zoomFactor-1 : -(1-1/zoomFactor))
-			var yDelta = svgViewboxHeight * (wheelMovement < 0 ? zoomFactor-1 : -(1-1/zoomFactor))
-			// zoom towards the current mouse position
-			var relX = (mouse[0]-svgViewboxX)/svgViewboxWidth // in [0,1]
-			var relY = (mouse[1]-svgViewboxY)/svgViewboxHeight // in [0,1]
-			svgViewboxX -= xDelta * relX
-			svgViewboxY -= yDelta * relY
-			svgViewboxWidth += xDelta
-			svgViewboxHeight += yDelta
-
-			svg.transition()
-				.duration(300)
-				.attr("viewBox", svgViewboxX+" "+svgViewboxY+" "+svgViewboxWidth+" "+svgViewboxHeight)
-
-			d3.event = null
-		}
-
-		// IE9, Chrome, Safari, Opera
-		domSvg.addEventListener("mousewheel", zoom, false)
-		// Firefox
-		domSvg.addEventListener("DOMMouseScroll", zoom, false)
-
-
+		//vikus.matrixToTable(result)
 	}
+
 
 
 	// global tools
